6b8958fc59f11ca8afb9c6e62cfd6574d6bf2c5b
akadijevic <akadijevic@css.edu>
2017-05-03 20:50:43 -0500

    Initial commit

---
+++ app/src/main/java/css3334/css/nerdfitness/MainActivity.java
@@ -0,0 +1,101 @@
+package css3334.css.nerdfitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    @Bind(R.id.btn_signout) Button _signoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkUserAuthenticated();
+
+        ButterKnife.bind(this);
+        /* Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent); */
+
+        _signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Signout();
+            }
+        });
+    }
+
+    private void checkUserAuthenticated() {
+        mAuth = FirebaseAuth.getInstance(); //declare object for Firebase
+        mAuthListener = new FirebaseAuth.AuthStateListener() { //initialized mAuthListener
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //track the user when they sign in or out using the firebaseAuth
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // User is signed out
+                    Log.d("CSS3334","onAuthStateChanged - User NOT is signed in");
+                    Intent signInIntent = new Intent(getBaseContext(), LoginActivity.class);
+                    startActivity(signInIntent);
+                }
+            }
+        };
+    }
+
+
+
+   public void Signout() { mAuth.signOut(); }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onStart() {
+        //initiate the authentication listener
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener); // update the listener on the users place
+    }
+
+    @Override
+    public void onStop() {
+        //discontinue the authentication
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener); // remove the listener
+        }
+    }
+}
