cff7c672dbcb3cd63a83aae07eb69426f1c6280f
akadijevic <akadijevic@css.edu>
2017-05-05 22:57:18 -0500

    Working camera :)

--- app/src/main/java/css3334/css/nerdfitness/NewPostActivity.java
+++ app/src/main/java/css3334/css/nerdfitness/NewPostActivity.java
@@ -7,7 +7,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,8 +17,21 @@ import android.widget.ImageView;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.util.Date;
+
+import static android.R.attr.data;
 
 
 /**
@@ -25,23 +40,17 @@ import com.google.firebase.database.FirebaseDatabase;
 public class NewPostActivity extends AppCompatActivity {
     Button myUploadButton;
     ImageView myImageView;
-    Intent cameraPickerIntent;
-
-    Camera camera;
 
     int CAMERA_REQUEST_CODE = 1;
-
-    DatabaseReference mstorage;
+    StorageReference mstorage;
     ProgressDialog mprogress;
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
-        mstorage = FirebaseDatabase.getInstance().getReference();
+        mstorage = FirebaseStorage.getInstance().getReference();
         mprogress = new ProgressDialog(this);
-
         myUploadButton = (Button) findViewById(R.id.upload);
         myImageView = (ImageView) findViewById(R.id.imageView);
 /* sets a method for upload button */
@@ -50,10 +59,8 @@ public class NewPostActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent cameraintent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-
-                startActivityForResult(cameraintent, 1337);
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
 
             }
         });
@@ -72,16 +79,51 @@ public class NewPostActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+
             mprogress.setMessage("Uploading Image");
             mprogress.show();
 
-            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-            myImageView.setImageBitmap(thumbnail);
-        }else {
-            Toast.makeText(NewPostActivity.this, "Picture Not taken", Toast.LENGTH_LONG).show();
-            //DatabaseReference filepath = mstorage.child("Photos").child(uri.getLastPathSegment())
+            Bundle extras = data.getExtras();
+            final Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] databaos = baos.toByteArray();
+
+            myImageView.setImageBitmap(bitmap);
+
+
+          // Uri uri = data.getData();
+            StorageReference filepath = mstorage.child("Photo" +new Date().getTime());
+
+             UploadTask uploadTask = filepath.putBytes(databaos);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    mprogress.dismiss();
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                   // Picasso.with(NewPostActivity.this).load(downloadUrl).fit().centerCrop().into(myImageView);
+                    //get the camera image
+
+
+                    /* Uri downloadUri= taskSnapshot.getDownloadUrl();
+                    Picasso.with(NewPostActivity.this).load(downloadUri).fit().centerCrop().into(myImageView);
+                    Toast.makeText(NewPostActivity.this, "Picture Taken", Toast.LENGTH_LONG).show(); */
+
+
+
+                }
+            });
+
+                    /* .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            }) */
+
+
         }
-        super.onActivityResult(requestCode, resultCode, data);
 
     }
 }
